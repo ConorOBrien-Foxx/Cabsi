81c2183e71cb21133096125b617faf2fc91deb42
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2023-12-02 15:17:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# Cabsi
+
+## Stack-based BASIC analogue
+
+(The name is a silly-sounding anagram of Basic.)
+
+A stack-based language with BASIC-esque instruction numbering and control flow.
+
+## Example
+
+```basic
+10  REM Fibonacci program
+20  PUSH 0
+30  PUSH 1
+40  PUSH "Input N: "
+50  INPUT
+60  JNP 150
+70  DEC      REM [3, 5, N-1]
+80  ROT      REM [5, N, 3]
+90  ROT      REM [N, 3, 5]
+100 DUP      REM [N, 3, 5, 5]
+110 ROT      REM [N, 5, 5, 3]
+120 ADD      REM [N, 5, 8]
+130 ROT      REM [5, 8, N]
+140 GOTO 60
+150 POP
+160 POP
+170 PRINT
+180 EXIT
+```
+
+## Commands
+
+- Separated by command types: Stack Population, Stack Manipulation, Control Flow, Miscellaneous.
+- Any symbols which follow the command are expected to be given with the command
+   - E.g. `GOTO lineNumber` expects a numeric word `lineNumber` following `GOTO`, like `GOTO 500`.
+- The numbers in parentheses after the command denotes how many items on the stack are required for a successful execution, and how many items are left on the stack relative to that
+   - E.g. `INPUT`(1 &rarr; 1) requires the stack to have at least item, and leaves the stack with 1 item, relatively speaking.
+
+### Stack Population
+
+#### `PUSH [x1, [x2, [...xN]]]`(0 &rarr; N)
+Pushes 0 or more items to the stack in sequence.
+
+#### `INPUT`(1 &rarr; 1)
+Pops a prompt string `S` and asks the user for input with `S`. Pushes the corresponding literal onto the stack. Prompt string not displayed (but still required) when not reading from the user's keyboard.
+
+### Stack Manipulation
+
+#### `ROT`(3 &rarr; 3)
+Rotates the top three items on the stack, pulling the third item from the top of the stack to the top of the stack. `[A, B, C] -> [B, C, A]`.
+
+#### `DUP`(1 &rarr; 2)
+Duplicates the top item on the stack.
+
+#### `POP`(1 &rarr; 0)
+Removes the top item from the stack.
+
+#### `SWAP`(2 &rarr; 2)
+Swaps the positions of the top two items on the stack.
+
+#### `OVER`(2 &rarr; 3)
+Copies the second item from the top and pushes it onto the top of the stack.
+
+### Control Flow
+
+#### `GOTO lineNumber`(0 &rarr; 0)
+Unconditionally jumps to the instruction at the specified `lineNumber`.
+
+#### `JP lineNumber`(0 &rarr; 0)
+Jumps to the instruction at the specified `lineNumber` if the top item on the stack is positive.
+
+#### `JNP lineNumber`(0 &rarr; 0)
+Jumps to the instruction at the specified `lineNumber` if the top item on the stack is not positive.
+
+#### `JN lineNumber`(0 &rarr; 0)
+Jumps to the instruction at the specified `lineNumber` if the top item on the stack is negative.
+
+#### `JNN lineNumber`(0 &rarr; 0)
+Jumps to the instruction at the specified `lineNumber` if the top item on the stack is not negative.
+
+#### `JZ lineNumber`(0 &rarr; 0)
+Jumps to the instruction at the specified `lineNumber` if the top item on the stack is zero.
+
+#### `GOSUB lineNumber`(0 &rarr; 0)
+Calls a subroutine located at the specified `lineNumber`.
+
+#### `RETURN`(0 &rarr; 0)
+Returns from the current subroutine.
+
+### Math
+
+#### `INC`(1 &rarr; 1)
+Increments the top item on the stack.
+
+#### `DEC`(1 &rarr; 1)
+Decrements the top item on the stack.
+
+#### `ADD`(2 &rarr; 1)
+Adds the top two items on the stack.
+
+#### `SUB`(2 &rarr; 1)
+Subtracts the top item on the stack from the second item.
+
+#### `MUL`(2 &rarr; 1)
+Multiplies the top two items on the stack.
+
+#### `DIV`(2 &rarr; 1)
+Divides the second item on the stack by the top item.
+
+#### `MOD`(2 &rarr; 1)
+Calculates the remainder of the division of the second item on the stack by the top item.
+
+#### `DIVMOD`(2 &rarr; 2)
+Calculates both the quotient and remainder of the division of the second item on the stack by the top item, pushing them sequentially
+
+### Miscellaneous
+
+#### `PRINT`(1 &rarr; 0)
+Prints the top item on the stack.
+
+#### `DEBUG`(0 &rarr; 0)
+Prints debugging information: Line number and stack information.
+
+#### `EXIT`(0 &rarr; 0)
+Exits the program.
---
+++ cabsi.js
@@ -0,0 +1,407 @@
+const splitParams = params => {
+    if(!params) {
+        return [];
+    }
+    
+    let i = 0;
+    let split = [];
+    let match, build = "";
+    while(i < params.length) {
+        if(match = params.slice(i).match(/^"(""|[^"])+?"/)) {
+            build += match[0];
+            i += match[0].length;
+        }
+        else {
+            build += params[i];
+            i++;
+        }
+        if(build.at(-1) === ",") {
+            split.push(build.slice(0, -1));
+            build = "";
+        }
+    }
+    split.push(build);
+    return split
+        .map(segment => segment.trim())
+        .filter(segment => !!segment);
+};
+
+const Instructions = {
+    // stack population
+    PUSH: 1,
+    INPUT: 2,
+    // stack manipulation
+    ROT: 10,    // A B C -> B C A
+    DUP: 11,
+    POP: 12,
+    SWAP: 13,
+    OVER: 14,
+    // control flow
+    GOTO: 20,
+    JP: 21, // jump positive
+    JNP: 22, // jump not positive
+    JN: 23, // jump negative
+    JNN: 24, // jump not negative
+    JZ: 25, // jump zero
+    GOSUB: 26, // go to subroutine
+    RETURN: 27, // return from subroutine
+    // math
+    INC: 30, // increment
+    DEC: 31, // decrement
+    ADD: 32,
+    SUB: 33,
+    MUL: 34,
+    DIV: 35,
+    MOD: 36,
+    DIVMOD: 37,
+    // misc
+    DEBUG: 50,
+    EXIT: 51,
+    PRINT: 52,
+};
+
+const tokenize = code => {
+    let parsed = code.split("\n")
+        .map(line => line.match(/(\d+)\s*(.*?)(?:\s*REM .*)?$/))
+        .filter(line => !!line);
+    let result = {};
+    for(let [ match, lineNumber, line ] of parsed) {
+        if(line) {
+            let [ , instruction, params ] = line.match(/(\w+)(?: (.*))?/);
+            const id = Instructions[instruction];
+            if(!id) {
+                console.warn("Unknown instruction", instruction);
+            }
+            result[lineNumber] = {
+                line,
+                lineNumber: parseInt(lineNumber, 10),
+                instruction,
+                id,
+                params: splitParams(params),
+            };
+            // console.log(result[lineNumber]);
+        }
+    }
+    let linePairs = Object.entries(result);
+    linePairs.sort((a, b) => a[0] - b[0]);
+    return linePairs.map(pair => pair[1]);
+};
+
+class CabsiInterpreter {
+    constructor(tokens) {
+        this.tokens = tokens;
+        this.iptr = 0;
+        this.stack = [];
+        this.callStack = [];
+        this.lineNumberToIndexMap = {};
+        this.tokens.forEach((token, idx) => {
+            this.lineNumberToIndexMap[token.lineNumber] = idx;
+        });
+    }
+    
+    hasStackSize(n) {
+        if(this.stack.length < n) {
+            console.error(`${this.token.instruction}@${this.token.lineNumber}: Expected ${n} entries on stack, got ${this.stack.length}`);
+            this.kill();
+            return false;
+        }
+        return true;
+    }
+    
+    async step() {
+        this.token = this.tokens[this.iptr];
+        let fn = CabsiInterpreter.InstructionMap[this.token.id];
+        if(fn) {
+            await fn.call(this);
+        }
+        else {
+            console.warn("Unimplemented instruction:", this.token.instruction);
+        }
+        this.iptr++;
+        // console.log(this.token.line, this.stack);
+    }
+    
+    async getInput(prompt="> ") {
+        if(!this.readline) {
+            this.readline = require("node:readline/promises");
+            const { stdin: input, stdout: output } = require('node:process');
+            this.rl = this.readline.createInterface({ input, output, terminal: false });
+        }
+        if(typeof process !== "undefined" && !process.stdin.isTTY) {
+            // do not show input prompt when reading from a pipe
+            prompt = "";
+        }
+        const answer = await this.rl.question(prompt);
+        return this.parseLiteral(answer);
+    }
+    
+    parseLiteral(raw) {
+        return JSON.parse(raw);
+    }
+    
+    push(...args) {
+        this.stack.push(...args);
+    }
+    
+    async run() {
+        while(this.iptr < this.tokens.length) {
+            await this.step();
+        }
+    }
+    
+    jumpToLine(lineNumber, decrement=true) {
+        let resultIndex = null;
+        if(Object.hasOwn(this.lineNumberToIndexMap, lineNumber)) {
+            resultIndex = this.lineNumberToIndexMap[lineNumber] ?? null;
+        }
+        else {
+            // binary search
+            let left = 0;
+            let right = this.tokens.length - 1;
+            while(left <= right) {
+                let middle = Math.floor((left + right) / 2);
+                // console.log(middle, left, right);
+                // console.log(this.tokens[middle].lineNumber, lineNumber, this.tokens[middle].lineNumber > lineNumber);
+                if(this.tokens[middle].lineNumber > lineNumber) {
+                    resultIndex = middle;
+                    right = middle - 1;
+                }
+                else {
+                    if(left === 0 && right === 0) {
+                        resultIndex = 0;
+                        break;
+                    }
+                    left = middle + 1;
+                }
+            }
+        }
+        
+        if(resultIndex === null) {
+            console.warn("Cannot jump to line", lineNumber);
+            return this.tokens.length;
+        }
+        
+        if(decrement) {
+            // decrement to counteract auto pointer increment
+            resultIndex--;
+        }
+        
+        this.iptr = resultIndex;
+    }
+    
+    static InstructionMap = {
+        [Instructions.PUSH]() {
+            for(let param of this.token.params) {
+                this.push(this.parseLiteral(param));
+            }
+        },
+        async [Instructions.INPUT]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            const prompt = this.stack.pop();
+            const input = await this.getInput(prompt);
+            this.push(input);
+        },
+        [Instructions.GOTO]() {
+            const targetLine = parseInt(this.token.params[0], 10);
+            this.jumpToLine(targetLine);
+        },
+        [Instructions.GOSUB]() {
+            const targetLine = parseInt(this.token.params[0], 10);
+            const myLine = this.token.lineNumber;
+            this.callStack.push(myLine);
+            this.jumpToLine(targetLine);
+        },
+        [Instructions.RETURN]() {
+            const returnLine = this.callStack.pop();
+            this.jumpToLine(returnLine + 1);
+        },
+        [Instructions.DEBUG]() {
+            let { lineNumber, instruction } = this.token;
+            console.log(`${lineNumber} ${instruction}`, this.stack);
+            // console.log(`${lineNumber} ${instruction} @ [ ${this.stack.join(" ")} ]`);
+        },
+        [Instructions.SWAP]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(b, a);
+        },
+        [Instructions.JP]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            if(this.stack.at(-1) > 0) {
+                const targetLine = parseInt(this.token.params[0], 10);
+                this.jumpToLine(targetLine);
+            }
+        },
+        [Instructions.JNP]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            if(this.stack.at(-1) <= 0) {
+                const targetLine = parseInt(this.token.params[0], 10);
+                this.jumpToLine(targetLine);
+            }
+        },
+        [Instructions.JN]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            if(this.stack.at(-1) < 0) {
+                const targetLine = parseInt(this.token.params[0], 10);
+                this.jumpToLine(targetLine);
+            }
+        },
+        [Instructions.JNN]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            if(this.stack.at(-1) >= 0) {
+                const targetLine = parseInt(this.token.params[0], 10);
+                this.jumpToLine(targetLine);
+            }
+        },
+        [Instructions.JZ]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            if(this.stack.at(-1) == 0) {
+                const targetLine = parseInt(this.token.params[0], 10);
+                this.jumpToLine(targetLine);
+            }
+        },
+        [Instructions.POP]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            this.stack.pop();
+        },
+        [Instructions.ROT]() {
+            if(!this.hasStackSize(3)) {
+                return;
+            }
+            let [ a, b, c ] = this.stack.splice(-3);
+            this.stack.push(b, c, a);
+        },
+        [Instructions.DUP]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            this.stack.push(this.stack.at(-1));
+        },
+        [Instructions.INC]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            this.push(this.stack.pop() + 1);
+        },
+        [Instructions.DEC]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            this.push(this.stack.pop() - 1);
+        },
+        [Instructions.ADD]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a + b);
+        },
+        [Instructions.SUB]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a - b);
+        },
+        [Instructions.MUL]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a * b);
+        },
+        [Instructions.DIV]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a / b);
+        },
+        [Instructions.MOD]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a % b);
+        },
+        [Instructions.DIVMOD]() {
+            if(!this.hasStackSize(2)) {
+                return;
+            }
+            let [ a, b ] = this.stack.splice(-2);
+            this.push(a / b);
+            this.push(a % b);
+        },
+        [Instructions.PRINT]() {
+            if(!this.hasStackSize(1)) {
+                return;
+            }
+            console.log(this.stack.pop());
+        },
+        [Instructions.EXIT]() {
+            this.kill();
+        },
+    };
+    
+    kill() {
+        this.iptr = this.tokens.length;
+        this.rl?.close();
+    }
+    
+    static fromString(code) {
+        return new CabsiInterpreter(tokenize(code));
+    }
+    
+    static async interpret(code) {
+        let interpreter = CabsiInterpreter.fromString(code);
+        await interpreter.run();
+        // console.log(interpreter.stack);
+        interpreter.kill();
+    }
+}
+
+const prog = `
+10 PUSH 7
+20 GOSUB 100
+30 DUP
+40 GOSUB 100
+50 DEBUG
+60 EXIT
+100 REM Fibonacci n -- n'
+110 PUSH 0, 1
+120 ROT
+130 JNP 220
+140 DEC
+150 ROT
+160 ROT
+170 DUP
+180 ROT
+190 ADD
+200 ROT
+210 GOTO 130
+220 POP
+230 POP
+240 RETURN
+`.trim();
+
+(async function main() {
+    let end = await CabsiInterpreter.interpret(prog);
+
+    // console.log(end);
+})();
